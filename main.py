5f8eda57025f01d94f50015c6cd84d52989829bb
SaiHimaja24 <86096811+SaiHimaja24@users.noreply.github.com>
2023-02-05 13:39:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flask_login
 #import flask_loginmanager
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mongoengine import mongoengine
-from flask_sqlalchemy.session import Session
+#from flask_sqlalchemy.session import Session
 from flask_wtf import FlaskForm, Form
 from wtforms import RadioField
 from wtforms.validators import DataRequired
